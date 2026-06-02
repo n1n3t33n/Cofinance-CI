9889a6d579714f1d955d1fae845b46e6f44fdd7d
n1n3t33n <9ine1een91@gmail.com>
2026-06-02 19:17:05 +0000

    initialisation du projet: structure du projet COFINANCE CI

---
+++ cofinance_ci/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for cofinance_ci project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Documentation API
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Applications
+    path('api/auth/',          include('accounts.urls')),
+    path('api/credits/',       include('credits.urls')),
+    path('api/repayments/',    include('repayments.urls')),
+    path('api/insurance/',     include('insurance.urls')),
+    path('api/notifications/', include('notifications.urls')),
+    path('api/chat/',          include('chat.urls')),
+    path('api/dashboard/',     include('dashboard.urls')),
+]
